d727eb507401a48caabcd9750e99c43133fe0627
Thorinka <mmlentyshenko@gmail.com>
2023-04-02 19:30:20 +0300

    refactor tests, add fixture

--- tests/tests_dicts.py
+++ tests/tests_dicts.py
@@ -1,7 +1,15 @@
+import pytest
+
 from utils import dicts
 
-def test_get_val():
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+
+@pytest.fixture
+def coll():
+    return {"vcs": "mercurial"}
+
+
+def test_get_val(coll):
+    assert dicts.get_val(coll, "vcs") == "mercurial"
+    assert dicts.get_val(coll, "vcs", "git") == "mercurial"
     assert dicts.get_val({}, "vcs", "git") == "git"
     assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
